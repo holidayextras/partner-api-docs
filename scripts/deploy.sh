4231f454e6ba05a7018b148f365f776932fbd752
Mark Terry <mark.terry@holidayextras.com>
2017-05-15 15:09:23 +0100

    updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e
 RELEASE_BRANCH='master'
-exit 1
 echo "Current branch: ${CURRENT_BRANCH}"
 if [ "${CURRENT_BRANCH}" != "${RELEASE_BRANCH}" ]; then
   echo "ERROR: Not on ${RELEASE_BRANCH} branch"
   exit 2
 fi
-git checkout ${RELEASE_BRANCH}
-git branch
-git add docs
+git checkout gh-pages
+git merge ${RELEASE_BRANCH}
+cp -a _site/* ./
+git add * || true
 git status
 if [[ -n $(git status -s) ]] ; then
-  echo "Updated dist needs committing"
+  echo "Updated site needs committing"
   git commit -m "Updated live site"
-  git push origin ${RELEASE_BRANCH}
+  git push origin gh-pages
 fi
+echo "...done"
