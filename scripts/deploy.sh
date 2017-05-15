2da32b8ff2a788d1998440fea793cec741931308
Mark Terry <mark.terry@holidayextras.com>
2017-05-15 15:12:23 +0100

    use travis env var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 RELEASE_BRANCH='master'
-echo "Current branch: ${CURRENT_BRANCH}"
-if [ "${CURRENT_BRANCH}" != "${RELEASE_BRANCH}" ]; then
+echo "Current branch: ${TRAVIS_BRANCH}"
+if [ "${TRAVIS_BRANCH}" != "${RELEASE_BRANCH}" ]; then
   echo "ERROR: Not on ${RELEASE_BRANCH} branch"
   exit 2
 fi
